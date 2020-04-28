b0c124f953331cf435e0b735d1bac8080486bf7e
devodev <abalexandrebarone@gmail.com>
2020-04-28 11:36:23 -0400

    Add hub scaffolding

---
+++ cmd/hub/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	loggerOutput  = os.Stderr
+	defaultOutput = os.Stdout
+)
+
+// Execute executes the root command.
+func Execute() error {
+	rootCmd := newCommandRoot()
+	return rootCmd.Execute()
+}
+
+func writeOut(line string) {
+	fmt.Fprintln(defaultOutput, line)
+}
+
+func newCommandRoot() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "hub",
+		Short:   "gRPC hub.",
+		Version: "0.1.0",
+	}
+	cmd.AddCommand()
+	return cmd
+}
+
+func main() {
+	if err := Execute(); err != nil {
+		writeOut(err.Error())
+		os.Exit(1)
+	}
+}
---
+++ cmd/hub/serve.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"google.golang.org/grpc"
+)
+
+// ServerConfig holds config for the Fluentd command.
+type ServerConfig struct {
+	ListenAddr string `envconfig:"LISTEN_ADDR" default:":8080"`
+}
+
+// NewServerConfig returns ServerConfig after being processed
+// using envconfig to set default values and environment variables.
+func NewServerConfig() *ServerConfig {
+	c := &ServerConfig{}
+	envconfig.Process("", c)
+	return c
+}
+
+// AddFlags adds flags to the provided flagset.
+func (c *ServerConfig) AddFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&c.ListenAddr, "listen", c.ListenAddr, "listening address.")
+}
+
+func newCommandServe() *cobra.Command {
+	config := NewServerConfig()
+	cmd := &cobra.Command{
+		Use:   "serve",
+		Short: "serve the gRPC hub.",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			l, err := net.Listen("tcp", config.ListenAddr)
+			if err != nil {
+				return err
+			}
+			server := grpc.NewServer()
+			return server.Serve(l)
+		},
+	}
+	config.AddFlags(cmd.Flags())
+	return cmd
+}
