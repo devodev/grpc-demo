a6c4989bf6360f1754e1064652e8c77feb3e2f16
devodev <abalexandrebarone@gmail.com>
2020-05-05 10:16:41 -0400

    refactor hub dialer logic and move it to hub package.

--- cmd/server/cmd/serve.go
+++ cmd/server/cmd/serve.go
@@ -1,83 +1,57 @@
 package cmd
 
 import (
-	"crypto/tls"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
 
-	"github.com/gorilla/websocket"
-	"github.com/hashicorp/yamux"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"google.golang.org/grpc"
 
 	"github.com/devodev/grpc-demo/internal/api"
 	"github.com/devodev/grpc-demo/internal/hub"
 )
 
-// ServerConfig holds config for the Fluentd command.
-type ServerConfig struct {
+// Config holds config for the Fluentd command.
+type Config struct {
 	HubAddr            string `envconfig:"HUB_ADDR" default:"ws://localhost:8080/ws"`
 	InsecureSkipVerify bool   `envconfig:"TLS_INSECURE_SKIP_VERIFY"`
 }
 
-// NewServerConfig returns ServerConfig after being processed
-// using envconfig to set default values and environment variables.
-func NewServerConfig() *ServerConfig {
-	c := &ServerConfig{}
+// SetupCmd set flags on the provided cmd and resolve env variables using the provided Config.
+func SetupCmd(cmd *cobra.Command, c *Config) *cobra.Command {
 	envconfig.Process("", c)
-	return c
-}
-
-// AddFlags adds flags to the provided flagset.
-func (c *ServerConfig) AddFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&c.HubAddr, "hub-uri", c.HubAddr, "hub websocket uri.")
-	fs.BoolVar(&c.InsecureSkipVerify, "tls-insecure-skip-verify", c.InsecureSkipVerify, "INSECURE: skip tls checks")
+	cmd.Flags().StringVar(&c.HubAddr, "hub-uri", c.HubAddr, "hub websocket uri.")
+	cmd.Flags().BoolVar(&c.InsecureSkipVerify, "tls-insecure-skip-verify", c.InsecureSkipVerify, "INSECURE: skip tls checks")
+	return cmd
 }
 
 func newCommandServe() *cobra.Command {
-	config := NewServerConfig()
+	var config Config
+
 	cmd := &cobra.Command{
-		Use:   "serve [name] [domain]",
+		Use:   "serve [name]",
 		Short: "serve the gRPC server.",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			name := args[0]
 
+			// ? TODO: I dont think we need to have a struct.
+			// ? TODO: Could move validation inside Dial()
+			// ? TODO: The use case I see that might be useful is
+			// ? TODO: provide helper methods to set hub specific headers.
+			hubDialer, err := hub.NewDialer(config.HubAddr, config.InsecureSkipVerify, name)
+			if err != nil {
+				return err
+			}
+			srvConn, err := hubDialer.DialAndWrap()
+			if err != nil {
+				return err
+			}
+
 			interrupt := make(chan os.Signal, 1)
 			signal.Notify(interrupt, os.Interrupt)
-
-			u, err := url.Parse(config.HubAddr)
-			if err != nil {
-				return err
-			}
-			dialer := websocket.DefaultDialer
-			if config.InsecureSkipVerify {
-				dialer.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-			}
-
-			header := make(http.Header)
-			header.Add("X-Hub-Meta-Name", name)
-			wsConn, _, err := dialer.Dial(u.String(), header)
-			if err != nil {
-				return err
-			}
-
-			wsRwc, err := hub.NewRWC(websocket.BinaryMessage, wsConn)
-			if err != nil {
-				wsConn.Close()
-				return err
-			}
-
-			srvConn, err := yamux.Server(wsRwc, yamux.DefaultConfig())
-			if err != nil {
-				wsRwc.CloseWithMessage(err.Error())
-				return err
-			}
 
 			server := grpc.NewServer()
 			fluentdService := &api.FluentdService{}
@@ -100,6 +74,6 @@ func newCommandServe() *cobra.Command {
 			return nil
 		},
 	}
-	config.AddFlags(cmd.Flags())
+	cmd = SetupCmd(cmd, &config)
 	return cmd
 }
